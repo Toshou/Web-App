83fd59a46241dd44ce5be212e758afe72e6c7511
Chloé <chloe.deperthes@gmail.com>
2023-03-20 17:05:42 +0100

    Update circleSportswear.js

--- server/eshops/circleSportswear.js
+++ server/eshops/circleSportswear.js
@@ -1,19 +1,22 @@
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const url = 'https://shop.circlesportswear.com/collections/collection-homme';
+const url = "https://shop.circlesportswear.com/collections/collection-homme";
 
-fetch(url)
-  .then(response => response.text())
-  .then(html => {
+axios
+  .get(url)
+  .then((response) => {
+    const html = response.data;
     const $ = cheerio.load(html);
     const products = [];
 
-    $('.product').each((i, el) => {
-      const name = $(el).find('.item__name').text().trim();
-      const price = $(el).find('.price').text().trim();
-      products.push({ item_name: name, price: price });
-    });
+    $("#product-grid")
+      .find("li")
+      .each((i, el) => {
+        const name = $(el).find("a.full-unstyled-link").first().text().trim();
+        const price = $(el).find(".money").first().text().trim();
+        products.push({ item_name: name, price: price });
+      });
 
     console.log(products);
   })
